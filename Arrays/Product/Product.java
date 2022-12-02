11f48f1521e828a97be3e4008250b483e01d3b60
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-12-02 11:31:35 +0100

    Add files via upload

---
+++ Arrays/Product/Product.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.Objects;
+
+public class Product {
+    private String name;
+
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void addProduct(List<Product> list, String name){
+        list.add(new Product(name));
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
