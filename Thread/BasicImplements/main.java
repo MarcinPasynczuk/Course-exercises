3b2665f4f08ce44f98e607471de83e8355110126
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-12-16 12:31:52 +0100

    Create main.java

---
+++ Thread/BasicImplements/main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        final BankAccount account = new BankAccount("123", 1000.00);
+
+Thread acc1 = new Thread(new Runnable() {
+    @Override
+    public void run() {
+        account.deposit(300.00);
+        account.withdraw(50.00);
+    }
+});
+
+
+Thread acc2 = new Thread(new Runnable() {
+    @Override
+    public void run() {
+        account.deposit(203.75);
+        account.withdraw(100.00);
+    }
+});
+
+acc1.start();
+acc2.start();
+
+    }}
