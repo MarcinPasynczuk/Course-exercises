da8f587125b29ba44da0e801ab8d6470c059db83
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2022-12-16 12:25:19 +0100

    Add files via upload

---
+++ Thread/BasicExtends/BankAccount.java
@@ -0,0 +1,23 @@
+class BankAccount extends Thread{
+
+    private double balance;
+    private String accountNumber;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public BankAccount(BankAccount account) {
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+
+}
