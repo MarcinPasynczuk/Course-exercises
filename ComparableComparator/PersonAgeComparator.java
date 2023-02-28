9156451d414e5e504f03271daa7721c0d22beff5
Marcin Pasynczuk <110817419+MarcinPasynczuk@users.noreply.github.com>
2023-02-28 16:29:55 +0100

    Update PersonAgeComparator.java

--- ComparableComparator/PersonAgeComparator.java
+++ ComparableComparator/PersonAgeComparator.java
@@ -4,6 +4,6 @@ public class PersonAgeComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
                 return  Integer.compare(o1.getAge(), o2.getAge());
-
+ 
     }
 }
